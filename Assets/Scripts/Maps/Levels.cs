c93077f5276bab2014d7b3eb3f21dd0865c95c2d
Richard Lee <leerw4@gmail.com>
2021-11-14 23:14:07 -0500

    Adding split to level

--- Assets/Scripts/Maps/Levels.cs
+++ Assets/Scripts/Maps/Levels.cs
@@ -3,12 +3,16 @@
     public static LevelTree getLevel1()
     {
         LevelTree level1 = new LevelTree("SpawnScene");
-        level1.root
+        LevelTreeNode r3 = level1.root
             .AddRoom(1, "StartScene")
-            .AddRoom(2, "Scene4")
-            .AddRoom(2, "BasicRoom")
+            .AddRoom(2, "Scene4");
+
+        r3.AddRoom(2, "BasicRoom")
             .AddRoom(4, "BasicRoom")
             .AddRoom(2, "FinalScene");
+
+        r3.AddRoom(3, "BasicRoom")
+            .AddRoom(3, "FinalScene");
 
         return level1;
     }
