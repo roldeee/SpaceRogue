a7d3536f02c0f18a0bd2ac81285fd0332fb26324
Richard Lee <leerw4@gmail.com>
2021-11-14 23:05:31 -0500

    Scene 4 dynamic doors

--- Assets/Scripts/Maps/Levels.cs
+++ Assets/Scripts/Maps/Levels.cs
@@ -5,7 +5,7 @@
         LevelTree level1 = new LevelTree("SpawnScene");
         level1.root
             .AddRoom(1, "StartScene")
-            .AddRoom(2, "Scene3")
+            .AddRoom(2, "Scene4")
             .AddRoom(2, "BasicRoom")
             .AddRoom(4, "BasicRoom")
             .AddRoom(2, "FinalScene");
