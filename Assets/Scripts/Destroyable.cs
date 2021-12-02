36775f5e6275f957a05e733e932ca301c294c743
Richard Lee <leerw4@gmail.com>
2021-12-01 23:12:57 -0500

    Destroyable pods in spawn scene

---
+++ Assets/Scripts/Destroyable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroyable : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Projectile projectile = collision.collider.GetComponent<Projectile>();
+        if (projectile != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
